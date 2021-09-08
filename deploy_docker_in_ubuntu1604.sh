a268aebbf479d1373c25f9cb03c8feb985b5eabf
natop <golfeng@gmail.com>
2021-09-08 17:30:56 +0800

    add

--- deploy_docker_in_ubuntu1604.sh
+++ deploy_docker_in_ubuntu1604.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-sudo apt-get update
+echo '198417us' | sudo -S apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+ sudo apt-get -y install docker-ce=5:19.03.0~3-0~ubuntu-xenial docker-ce-cli=5:19.03.0~3-0~ubuntu-xenial containerd.io
